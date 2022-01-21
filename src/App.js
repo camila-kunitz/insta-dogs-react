093993b6e194e1287df059a7f8e802ece397774f
Camila Kunitz <camila.kunitz@gmail.com>
2022-01-21 13:49:33 -0300

    Cria rotas iniciais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Api from "./api/Api";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
-      <Api />
-      </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
