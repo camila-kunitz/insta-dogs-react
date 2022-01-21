db96a448145fe611b33d17def310f180598f3f1d
Camila Kunitz <camila.kunitz@gmail.com>
2022-01-21 10:19:22 -0300

    Cria requisições para API Dogs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import './App.css';
+import Api from "./api/Api";
 
 function App() {
   return (
-    <div> App React </div>
+    <div>
+      <Api />
+      </div>
   );
 }
 
