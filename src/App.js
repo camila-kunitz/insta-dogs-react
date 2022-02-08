b007997b12329d825f8f1858052303e497f1773c
Camila Kunitz <camila.kunitz@gmail.com>
2022-02-08 15:46:44 -0300

    Cria e estiliza página específica de foto

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext'
+import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -18,14 +19,15 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
-            <Route 
-              path="conta/*" 
+            <Route
+              path="conta/*"
               element={
                 <ProtectedRoute>
                   <User />
                 </ProtectedRoute>
-              } 
+              }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
