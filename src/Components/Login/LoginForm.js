846522c56b6fe9bcec72a4537a99cd5cb44ce936
Camila Kunitz <camila.kunitz@gmail.com>
2022-02-09 14:23:04 -0300

    Cria Head de páginas

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -7,8 +7,7 @@ import { UserContext } from '../../UserContext';
 import Error from '../Helper/Error';
 import styles from './LoginForm.module.css';
 import stylesBtn from '../Forms/Button.module.css';
-
-
+import Head from '../Helper/Head';
 
 const LoginForm = () => {
   const username = useForm();
@@ -16,39 +15,39 @@ const LoginForm = () => {
 
   const { userLogin, error, loading } = React.useContext(UserContext);
 
-async function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    if(username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
     }
   }
 
-
   return (
-  <section className="animeLeft">
-    <h1 className="title">Login</h1>
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <Input label="Usuário" type="text" name="username" {...username} />
-      <Input label="Senha" type="password" name="password" {...password} />
-      {loading ? (
-        <Button disabled>Carregando...</Button>
-      ) : ( 
-      <Button>Entrar</Button>
-      )}
-      <Error error={error} />
-    </form>
-    <Link className={styles.perdeu} to="/login/perdeu">
-      Perdeu a Senha?
-    </Link>
-    <div className={styles.cadastro}>
-      <h2 className={styles.subtitle}>Cadastre-se</h2>
-      <p>Ainda não possui conta? Cadastre-se no site.</p>
-    <Link className={stylesBtn.button} to="/login/criar">
-      Cadastro
-    </Link>
-    </div>
-  </section>
+    <section className="animeLeft">
+      <Head title="Login" />
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        <Error error={error} />
+      </form>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
+    </section>
   );
 };
 
--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -7,19 +7,19 @@ import Button from '../Forms/Button';
 import Error from '../Helper/Error';
 import { PHOTO_POST } from '../../Api';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const UserPhotoPost = () => {
   const nome = useForm();
   const peso = useForm('number');
   const idade = useForm('number');
   const [img, setImg] = React.useState({});
-  const {data, error, loading, request} = useFetch();
+  const { data, error, loading, request } = useFetch();
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    if(data) navigate('/conta');
+    if (data) navigate('/conta');
   }, [data, navigate]);
-
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,37 +30,45 @@ const UserPhotoPost = () => {
     formData.append('idade', idade.value);
 
     const token = window.localStorage.getItem('token');
-    const {url, options} = PHOTO_POST(formData, token);
-    request(url, options); 
+    const { url, options } = PHOTO_POST(formData, token);
+    request(url, options);
   }
 
   function handleImgChange({ target }) {
     setImg({
-      preview:URL.createObjectURL(target.files[0]),
+      preview: URL.createObjectURL(target.files[0]),
       raw: target.files[0],
     });
   }
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
+      <Head title="Poste sua foto" />
+
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" nome="nome" {...nome} />
         <Input label="Peso" type="number" nome="peso" {...peso} />
         <Input label="Idade" type="number" nome="idade" {...idade} />
-        <input className={styles.file} type="file" nome="img" id="img" onChange={handleImgChange} />
+        <input
+          className={styles.file}
+          type="file"
+          nome="img"
+          id="img"
+          onChange={handleImgChange}
+        />
         {loading ? (
-            <Button disabled>Enviando...</Button>
-          ) : (
-            <Button>Enviar</Button>
-          )}
-          <Error error={error} />
+          <Button disabled>Enviando...</Button>
+        ) : (
+          <Button>Enviar</Button>
+        )}
+        <Error error={error} />
       </form>
       <div>
         {img.preview && (
-        <div 
-          className={styles.preview} 
-          style={{ backgroundImage: `url('${img.preview}')` }}
-        ></div>
+          <div
+            className={styles.preview}
+            style={{ backgroundImage: `url('${img.preview}')` }}
+          ></div>
         )}
       </div>
     </section>
